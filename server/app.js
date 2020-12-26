3df7f3667da680f07d5377a066ff860d9b7420d3
amira <xiaozhanoppa5101991@gmail.com>
2020-12-26 23:13:18 +0200

    --

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//write your code 
+app.get('/user',(req,res)=>{
+    var n=req.params[0];
+    var m=req.params[1] ;
+    res.send(n + ' ' + m) ;
+}) ;
+
+
+app.listen(3001,()=>{
+   console.log('server running on port 3000'); 
+});
+
