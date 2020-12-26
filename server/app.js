e303aaeab29d13270cd54f4e9ec2939518f6d2cd
amira <xiaozhanoppa5101991@gmail.com>
2020-12-27 00:01:19 +0200

    -

--- server/app.js
+++ server/app.js
@@ -4,16 +4,18 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-//write your code 
+ 
 app.get('/user',(req,res)=>{
-    var n=req.params[0];
-    var m=req.params[1] ;
-    res.send(n + ' ' + m) ;
+    const data = req.param('name') ;
+    res.json({message :"hello "+ '' + data}) ;
 }) ;
+/*app.post('/user',(req,res)=>{
+    res.json({message:"hello" +''+ req.body.user.name) ;
+}) ;*/
+
 
 
 app.listen(3001,()=>{
-   console.log('server running on port 3000'); 
+   console.log('server running on port 3001'); 
 });
 
